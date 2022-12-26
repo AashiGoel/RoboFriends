e8c7f8c327c0d5818ff6a4dbc3b9d1b1104ff429
AashiGoel <aashigoel71203@gmail.com>
2022-12-26 17:16:53 +0530

    Changed the document title

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App.js';
-
+import { Helmet } from 'react-helmet';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
+    <Helmet>
+      <title>RoboFriends</title>
+    </Helmet>
     <App />
+    </>
   </React.StrictMode>
 );
 
